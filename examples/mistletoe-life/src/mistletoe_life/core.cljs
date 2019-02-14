485543b0e0284882a5ad130c78d58b172d29fcba
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-02-14 19:18:25 +0200

    Fix swap! update fn, add borders.

--- examples/mistletoe-life/src/mistletoe_life/core.cljs
+++ examples/mistletoe-life/src/mistletoe_life/core.cljs
@@ -2,6 +2,8 @@
   (:require [mistletoe.signal :as sgn :refer [smap]]
             [mistletoe.dom :refer [el]]))
 
+;;;;
+
 (def ^:private live true)
 (def ^:private dead false)
 
@@ -11,21 +13,28 @@
 (defn- empty-grid [width height]
   (mapv (fn [_] (empty-row width)) (range height)))
 
+;;;;
+
 (def ^:private state (sgn/source (empty-grid 100 100)))
+
+;;;;
 
 (defn- row-view [i row]
   (el :tr
       (for [[j cell] (map-indexed (fn [j _] [j (smap #(get % j) row)]) @row)]
         (el :td
-            :style {:width "10px"
-                    :height "10px"
+            :style {:width "10px", :height "10px"
+                    :border "1px solid black"
                     :background-color (smap #(if % "black" "none") cell)}
-            :onclick (fn [_] (swap! state :update-in [i j] not))))))
+            :onclick (fn [_] (swap! state update-in [i j] not))))))
 
 (defn- ui-main [state]
   (el :table
+      :style {:border-collapse "collapse"}
       (for [[i row] (map-indexed (fn [i _] [i (smap #(get % i) state)]) @state)]
         (row-view i row))))
+
+;;;;
 
 (defn main []
   (.. js/document
