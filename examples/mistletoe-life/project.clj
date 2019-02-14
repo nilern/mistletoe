5e6f118aae9016f44182a96cf2daa1328bd14895
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-02-14 17:51:22 +0200

    Add trivial example project.

---
+++ examples/mistletoe-life/project.clj
@@ -0,0 +1,20 @@
+(defproject mistletoe-life "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :repositories [["jitpack" "https://jitpack.io"]]
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.516"]
+                 [com.github.nilern/mistletoe "master-SNAPSHOT"]]
+
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds [{:id           "dev"
+                        :source-paths ["src"]
+                        :compiler     {:output-to  "resources/public/js/main.js"
+                                       :output-dir "resources/public/js"
+                                       :main       mistletoe-life.core
+                                       :asset-path "js"}}]})
---
+++ examples/mistletoe-life/src/mistletoe_life/core.cljs
@@ -0,0 +1,4 @@
+(ns mistletoe-life.core)
+
+(defn main []
+  (.write js/document "Hello!"))
