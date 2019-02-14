8507b43000c361e4e702dee2bcce6f418a7a08b4
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-02-14 18:39:35 +0200

    Add game of life initial setup for example.

--- examples/mistletoe-life/project.clj
+++ examples/mistletoe-life/project.clj
@@ -8,7 +8,7 @@
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.516"]
-                 [com.github.nilern/mistletoe "master-SNAPSHOT"]]
+                 [com.github.nilern/mistletoe "05a8c1d"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
   :hooks [leiningen.cljsbuild]
--- examples/mistletoe-life/src/mistletoe_life/core.cljs
+++ examples/mistletoe-life/src/mistletoe_life/core.cljs
@@ -1,4 +1,33 @@
-(ns mistletoe-life.core)
+(ns mistletoe-life.core
+  (:require [mistletoe.signal :as sgn :refer [smap]]
+            [mistletoe.dom :refer [el]]))
+
+(def ^:private live true)
+(def ^:private dead false)
+
+(defn- empty-row [width]
+  (mapv (constantly dead) (range width)))
+
+(defn- empty-grid [width height]
+  (mapv (fn [_] (empty-row width)) (range height)))
+
+(def ^:private state (sgn/source (empty-grid 100 100)))
+
+(defn- row-view [i row]
+  (el :tr
+      (for [[j cell] (map-indexed (fn [j _] [j (smap #(get % j) row)]) @row)]
+        (el :td
+            :style {:width "10px"
+                    :height "10px"
+                    :background-color (smap #(if % "black" "none") cell)}
+            :onclick (fn [_] (swap! state :update-in [i j] not))))))
+
+(defn- ui-main [state]
+  (el :table
+      (for [[i row] (map-indexed (fn [i _] [i (smap #(get % i) state)]) @state)]
+        (row-view i row))))
 
 (defn main []
-  (.write js/document "Hello!"))
+  (.. js/document
+      (getElementById "app-root")
+      (appendChild (ui-main state))))
