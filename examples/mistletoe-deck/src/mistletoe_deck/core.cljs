8648746d8e86de205433bb41a17426c08dcd92b4
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-02-27 19:23:36 +0200

    Add Fisher-Yates shuffle to Deck demo.

--- examples/mistletoe-deck/src/mistletoe_deck/core.cljs
+++ examples/mistletoe-deck/src/mistletoe_deck/core.cljs
@@ -3,6 +3,16 @@
             [mistletoe.signal.util :refer [map-index-cached]]
             [mistletoe.dom :refer [el append-child!]]))
 
+(defn- rand [min max]
+  (->> (.random js/Math)
+       (* (- max min))                                      ; zoom
+       (.floor js/Math)                                     ; clip
+       (+ min)))                                            ; translate
+
+(def ^:private framerate 3)
+
+;;;;
+
 (defn- rank-symbol [rank]
   (case rank
     1 "A"
@@ -25,6 +35,10 @@
    :hearts   "crimson"
    :spades   "black"})
 
+(def ^:private nsuits (count suits))
+(def ^:private nranks 13)
+(def ^:private deck-len (* nsuits nranks))
+
 (defrecord Card [suit rank]
   Object
   (toString [_] (str (rank-symbol rank) (suit-symbol suit))))
@@ -32,25 +46,49 @@
 ;;;;
 
 (def ^:private state
-  (sgn/source {:phase :select
-               :deck  (for [suit suits
-                            rank (range 1 14)]
-                        (->Card suit rank))}))
+  (sgn/source {:phase       :select
+               :phase-state nil
+               :deck        (into [] (for [suit suits
+                                           rank (range 1 (inc nranks))]
+                                       (->Card suit rank)))}))
+
+(def ^:private editable? (smap #(= (:phase %) :select) state))
 
 ;;;;
 
 (defn- card-view [i card]
   (el :div :class "card"
-      :style {:color     (smap (comp suit-color :suit) card)
-              :transform (str "translate(" (* 35 i) "px)")}
+      :style {:color      (smap (comp suit-color :suit) card)
+              :transform  (str "translate(" (* 35 i) "px)")
+              :transition (str "transform " (/ 1000 framerate 2) "ms ease-in-out")}
       (smap str card)))
 
 (defn- ui-main [state]
   (let [deck (smap :deck state)]
     (el :div
-        (->> deck
-             (smap (partial map-indexed (fn [i _] [i (smap #(nth % i nil) deck)])))
-             (smap (map-index-cached (fn [[i card]] (card-view i card))))))))
+        (el :div
+            (el :input :type "button" :value "Fisher-Yates shuffle"
+                :onclick (fn [_]
+                           (let [step (fn [] (swap! state
+                                                    (fn [state]
+                                                      (let [i (-> state :phase-state :index)]
+                                                        (if (< i (dec deck-len))
+                                                          (let [j (rand i deck-len)]
+                                                            (-> state
+                                                                (assoc-in [:deck i] (get-in state [:deck j]))
+                                                                (assoc-in [:deck j] (get-in state [:deck i]))
+                                                                (update-in [:phase-state :index] inc)))
+                                                          (do (.clearInterval js/window (:interval-id state))
+                                                              (assoc state :phase :select, :phase-state nil)))))))]
+                             (when @editable?
+                               (swap! state assoc :phase :fisher-yates)
+                               (swap! state assoc :phase-state {:index 0})
+                               (swap! state assoc :interval-id (.setInterval js/window step (/ 1000 framerate))))))))
+
+        (el :div
+            (->> deck
+                 (smap (partial map-indexed (fn [i _] [i (smap #(nth % i nil) deck)])))
+                 (smap (map-index-cached (fn [[i card]] (card-view i card)))))))))
 
 ;;;;
 
