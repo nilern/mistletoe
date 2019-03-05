828fe637398bd6e4da294cc1a1a109a5015c8385
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-03-05 17:47:52 +0200

    Enable use of custom equality functions in signal graphs.

--- src/mistletoe/signal.cljs
+++ src/mistletoe/signal.cljs
@@ -3,7 +3,7 @@
 
 ;;;; # Source
 
-(deftype SourceSignal [^:mutable value ^:mutable watches]
+(deftype SourceSignal [^:mutable value, equals?, ^:mutable watches]
   IDeref
   (-deref [_] value)
 
@@ -12,8 +12,7 @@
   (-reset! [self value*]
     (let [old-value value]
       (set! value value*)
-      (when (not= old-value value*)                         ; This condition is why we don't just use cljs.core/Atom.
-        (-notify-watches self old-value value*))
+      (-notify-watches self old-value value*)
       value*))
 
 
@@ -26,19 +25,25 @@
 
   IWatchable
   (-notify-watches [self old-val new-val]
-    (doseq [[k f] watches]
-      (f k self old-val new-val)))
+    (when-not (equals? old-val new-val)                     ; This condition is why we don't just use cljs.core/Atom.
+      (doseq [[k f] watches]
+        (f k self old-val new-val))))
 
-  (-add-watch [_ k f]
-    (set! watches (assoc watches k f)))
+  (-add-watch [_ k f] (set! watches (assoc watches k f)))
 
   (-remove-watch [_ k] (set! watches (dissoc watches k))))
 
-(defn source
+(defn source*
   "A source signal with initial `value`; can be updated (with `reset!` or `swap!`).
-  Unlike normal atom, will not notify watchers if value is updated but does not actually change."
+  Unlike normal atom, will not notify watchers if value is updated but does not actually change
+  (according to the equality function `equals?`)."
+  [equals? value]
+  (SourceSignal. value equals? {}))
+
+(defn source
+  "Like [[source*]], but the equality function is always [[=]]."
   [value]
-  (SourceSignal. value {}))
+  (source* = value))
 
 ;;;; # Constant
 
@@ -72,7 +77,7 @@
       (when (not= old-val new-val)
         (-notify-watches self old-val new-val)))))
 
-(deftype DerivedSignal [f dependencies ^:mutable value ^:mutable watches]
+(deftype DerivedSignal [f, dependencies, ^:mutable value, equals?, ^:mutable watches]
   IDeref
   (-deref [_] value)
 
@@ -80,8 +85,9 @@
   IWatchable
   (-notify-watches [self old-val new-val]
     ;; TODO: DRY:
-    (doseq [[k g] watches]
-      (g k self old-val new-val)))
+    (when-not (equals? old-val new-val)
+      (doseq [[k g] watches]
+        (g k self old-val new-val))))
 
   (-add-watch [self k g]
     (when (empty? watches)
@@ -99,8 +105,15 @@
       (doseq [dependency dependencies]
         (-remove-watch dependency self)))))
 
-(defn smap
+(defn smap*
   "A derived signal; its value is always equal to `(apply f (map deref signals))`.
+  Will only notify watchers if the output value actually changed (according to the equality function `equals?`).
   Will only keep watches in `signals` if has watches itself so an impure `f` is not recommended."
+  [equals? f & signals]
+  (DerivedSignal. f signals #_OPTIMIZE (apply f (map deref signals)) equals? {}))
+
+(defn smap
+  "Like [[smap*]] but the equality function is always [[=]]."
   [f & signals]
-  (DerivedSignal. f signals #_OPTIMIZE (apply f (map deref signals)) {}))
+  (apply source* = f signals))
+
