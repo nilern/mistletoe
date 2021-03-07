e987481684eee41304337240629ee0d773bd9b8f
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-03-07 19:22:18 +0200

    Delete test app stuff as .core

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject mistletoe "0.1.0-SNAPSHOT"
-  :description "Dom magic box"
-  :url "http://github.com/nilern/mistletoe"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :dependencies [])
